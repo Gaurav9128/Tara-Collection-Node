2723dcffa39e78f2a17b258452ed630be8240c25
Gaurav Jain <139974825+Gaurav9128@users.noreply.github.com>
2025-01-20 09:31:47 +0530

    Update productRoute.js

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,37 +1,12 @@
-import express from 'express';
-import { 
-  listProducts, 
-  addProduct, 
-  removeProduct, 
-  singleProduct, 
-  updateProductPrice // Import the new controller
-} from '../controllers/productController.js';
+import express from 'express'
+import { listProducts, addProduct, removeProduct, singleProduct } from '../controllers/productController.js';
 import upload from '../middleware/multer.js';
 
 const productRouter = express.Router();
 
-// Route to add a product with multiple images
-productRouter.post(
-  '/add',
-  upload.fields([
-    { name: 'image1', maxCount: 1 },
-    { name: 'image2', maxCount: 1 },
-    { name: 'image3', maxCount: 1 },
-    { name: 'image4', maxCount: 1 }
-  ]),
-  addProduct
-);
+productRouter.post('/add',upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1},{name:'image3',maxCount:1},{name:'image4',maxCount:1}]),addProduct);
+productRouter.post('/remove',removeProduct);
+productRouter.post('/single',singleProduct);
+productRouter.get('/list',listProducts);
 
-// Route to remove a product
-productRouter.post('/remove', removeProduct);
-
-// Route to get a single product
-productRouter.post('/single', singleProduct);
-
-// Route to list all products
-productRouter.get('/list', listProducts);
-
-// New Route: Update product price
-productRouter.post('/update-price', updateProductPrice);
-
-export default productRouter;
+export default productRouter
