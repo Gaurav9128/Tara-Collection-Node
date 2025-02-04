032ac4230aa00f14fd67297ec574575999938e68
Gaurav Jain <139974825+Gaurav9128@users.noreply.github.com>
2025-02-04 09:43:05 +0530

    Update paymentRoute.js

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { createOrder, checkPaymentStatus } from '../controllers/paymentController.js';
+import { placeOrder, checkPaymentStatus } from '../controllers/paymentController.js';
 
 const router = express.Router();
 
-router.post('/create-order', createOrder);
+router.post('/create-order',  placeOrder);
 router.post('/status', checkPaymentStatus);
 
 export default router;
