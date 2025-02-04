a75f4d8ea3bfbe1e70eba50920d7151d848bb856
Gaurav Jain <139974825+Gaurav9128@users.noreply.github.com>
2025-02-04 09:45:30 +0530

    Update orderModel.js

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,3 +1,5 @@
+import mongoose from 'mongoose'
+
 const orderSchema = new mongoose.Schema({
     userId: { type: String, required: true },
     items: { type: Array, required: true },
@@ -7,8 +9,8 @@ const orderSchema = new mongoose.Schema({
     paymentMethod: { type: String, required: true },
     payment: { type: Boolean, required: true , default: false },
     date: {type: Number, required:true},
-    orderId: { type: String, required: true, unique: true }  // ✅ New field added
+    orderId: { type: String, required: true, unique: true }
 });
 
-const orderModel = mongoose.models.order || mongoose.model('order',orderSchema);
+const orderModel = mongoose.models.order || mongoose.model('order',orderSchema)
 export default orderModel;
