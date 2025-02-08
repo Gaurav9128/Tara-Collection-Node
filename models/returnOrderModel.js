baa3bf7a28930b91cf93152fa96be161b70821a2
Gaurav Jain <139974825+Gaurav9128@users.noreply.github.com>
2025-02-08 09:56:10 +0530

    Update returnOrderModel.js

--- models/returnOrderModel.js
+++ models/returnOrderModel.js
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
-
+ 
 const returnOrderSchema = new mongoose.Schema({
-    choice: { type: String, enum: ["refund", "exchange"], required: true }, // Ensuring it's either 'refund' or 'replacement'
+    choice: { type: String, enum: ["refund", "replacement"], required: true }, // Ensuring it's either 'refund' or 'replacement'
     orderId: { type: mongoose.Schema.Types.ObjectId, ref: "Order", required: true },
     productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
     reason: { type: String, required: true },
     userId: { type: String, required: true },  // Storing userId as a string
     status: { type: String, enum: ["Pending", "Approved", "Rejected"], default: "Pending" }, // Default status is 'Pending'
+    name: { type: String, required: true },
+    mobile: { type: String, required: true },
+    address: { type: String, required: true },
 }, { timestamps: true });
-
+ 
 const returnOrderModel = mongoose.model("ReturnOrder", returnOrderSchema);
-
+ 
 export default returnOrderModel;
