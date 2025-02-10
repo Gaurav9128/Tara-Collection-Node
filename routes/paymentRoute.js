482556217aefc1f0eceeb65f3621c0e07ee29941
Gaurav Jain <jaingaurav212000@gmail.com>
2025-02-10 07:58:43 +0530

    Removed old files and added new ones

--- routes/paymentRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createOrder, checkPaymentStatus } from '../controllers/paymentController.js';
-
-const router = express.Router();
-
-router.post('/create-order',  createOrder);
-router.post('/status', checkPaymentStatus);
-
-export default router;
